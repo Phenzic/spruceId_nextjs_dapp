ce784310fab686591cf11062272fbdb17f1ca0d0
Phenzic <juliuayoola5@gmail.com>
2024-02-05 18:14:56 +0100

    spruce_next_dapp

---
+++ app/components/SSXComponent.tsx
@@ -0,0 +1,59 @@
+"use client";
+import { SSX } from "@spruceid/ssx";
+import { useState } from "react";
+
+const SSXComponent = () => {
+
+  const [ssxProvider, setSSX] = useState<SSX | null>(null);
+
+  const ssxHandler = async () => {
+    const ssx = new SSX({
+      providers: {
+        server: {
+          host: "http://localhost:3000/api"
+        }
+      },
+    });
+    await ssx.signIn();
+    setSSX(ssx);
+  };
+
+  const ssxLogoutHandler = async () => {
+    ssxProvider?.signOut();
+    setSSX(null);
+  };
+
+  const address = ssxProvider?.address() || '';
+
+  return (
+    <>
+      <h2>User Authorization Module</h2>
+      <p>Authenticate and Authorize using your ETH keys</p>
+      <br></br>
+      {
+        ssxProvider ?
+          <>
+            {
+              address &&
+              <p>
+                <b>Ethereum Address:</b> <code>{address}</code>
+              </p>
+            }
+            <br />
+            <button onClick={ssxLogoutHandler}>
+              <span>
+                Sign-Out
+              </span>
+            </button>
+          </> :
+          <button onClick={ssxHandler}>
+            <span>
+              Sign-In with Ethereum
+            </span>
+          </button>
+      }
+    </>
+  );
+};
+
+export default SSXComponent;
